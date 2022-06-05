5fb27a44aa3497500702107261e70cc4c83a5de1
Alves <andre-ricardo.alves@capgemini.com>
2022-06-05 15:49:38 +0100

    Testing Order Service domain logic - PART 1

---
+++ order-service/order-domain/order-application-service/src/test/java/com/food/ordering/system/order/service/domain/UUIDTest.java
@@ -0,0 +1,12 @@
+package com.food.ordering.system.order.service.domain;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.UUID;
+
+public class UUIDTest {
+    @Test
+    public void createUUI(){
+        System.out.println(UUID.randomUUID());
+    }
+}
