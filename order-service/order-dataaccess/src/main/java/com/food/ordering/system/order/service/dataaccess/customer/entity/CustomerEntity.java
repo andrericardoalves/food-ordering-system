c75a3947895811fb620131fe3a4ac1d32437db51
Alves <andre-ricardo.alves@capgemini.com>
2022-06-08 22:54:10 +0100

    Data access module: Adding Customer and Restaurant implementations

---
+++ order-service/order-dataaccess/src/main/java/com/food/ordering/system/order/service/dataaccess/customer/entity/CustomerEntity.java
@@ -0,0 +1,21 @@
+package com.food.ordering.system.order.service.dataaccess.customer.entity;
+
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.util.UUID;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "orders_customer_m_views", schema = "customer")
+@Entity
+public class CustomerEntity {
+
+    @Id
+    private UUID id;
+}
