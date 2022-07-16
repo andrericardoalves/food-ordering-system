20ff5dc403dd7d04de13034cfca786453f264669
Alves <andre-ricardo.alves@capgemini.com>
2022-07-16 18:54:20 +0100

    Application Service Domain module: Implementing input ports - Part 2

--- payment-service/payment-domain/payment-application-service/src/main/java/com/food/ordering/system/payment/service/domain/ports/input/message/listener/PaymentRequestMessageListener.java
+++ payment-service/payment-domain/payment-application-service/src/main/java/com/food/ordering/system/payment/service/domain/ports/input/message/listener/PaymentRequestMessageListener.java
@@ -1,10 +1,11 @@
 package com.food.ordering.system.payment.service.domain.ports.input.message.listener;
 
+import com.food.ordering.system.payment.service.domain.dto.PaymentRequest;
 import com.food.ordering.system.payment.service.domain.entity.Payment;
 
 public interface PaymentRequestMessageListener {
 
-    void completePayment(Payment payment);
+    void completePayment(PaymentRequest paymentRequest);
 
-    void cancelPayment(Payment payment);
+    void cancelPayment(PaymentRequest paymentRequest);
 }
