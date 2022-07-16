475d19739b5e19456a0fc55483e815ed4f3e2cf6
Alves <andre-ricardo.alves@capgemini.com>
2022-07-16 20:08:27 +0100

    Application Service Domain module: Refactoring fire event process

--- payment-service/payment-domain/payment-application-service/src/main/java/com/food/ordering/system/payment/service/domain/ports/input/message/listener/PaymentRequestMessageListener.java
+++ payment-service/payment-domain/payment-application-service/src/main/java/com/food/ordering/system/payment/service/domain/ports/input/message/listener/PaymentRequestMessageListener.java
@@ -1,7 +1,6 @@
 package com.food.ordering.system.payment.service.domain.ports.input.message.listener;
 
 import com.food.ordering.system.payment.service.domain.dto.PaymentRequest;
-import com.food.ordering.system.payment.service.domain.entity.Payment;
 
 public interface PaymentRequestMessageListener {
 
