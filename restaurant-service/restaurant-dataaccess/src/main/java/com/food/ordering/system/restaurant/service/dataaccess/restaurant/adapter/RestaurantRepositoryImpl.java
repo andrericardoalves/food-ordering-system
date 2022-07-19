35168488aa8e62b54290094c574651d32e6409a0
Alves <andre-ricardo.alves@capgemini.com>
2022-07-19 18:38:55 +0100

    Implementing Data Access module

--- restaurant-service/restaurant-dataaccess/src/main/java/com/food/ordering/system/restaurant/service/dataaccess/restaurant/adapter/RestaurantRepositoryImpl.java
+++ restaurant-service/restaurant-dataaccess/src/main/java/com/food/ordering/system/restaurant/service/dataaccess/restaurant/adapter/RestaurantRepositoryImpl.java
@@ -1,10 +1,10 @@
-package com.food.ordering.system.order.service.dataaccess.restaurant.adapter;
+package com.food.ordering.system.restaurant.service.dataaccess.restaurant.adapter;
 
 import com.food.ordering.system.dataaccess.restaurant.entity.RestaurantEntity;
 import com.food.ordering.system.dataaccess.restaurant.repository.RestaurantJpaRepository;
-import com.food.ordering.system.order.service.dataaccess.restaurant.mapper.RestaurantDataAccessMapper;
-import com.food.ordering.system.order.service.domain.entity.Restaurant;
-import com.food.ordering.system.order.service.domain.ports.output.repository.RestaurantRepository;
+import com.food.ordering.system.restaurant.service.dataaccess.restaurant.mapper.RestaurantDataAccessMapper;
+import com.food.ordering.system.restaurant.service.domain.entity.Restaurant;
+import com.food.ordering.system.restaurant.service.domain.ports.output.repository.RestaurantRepository;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -12,17 +12,16 @@ import java.util.Optional;
 import java.util.UUID;
 
 @Component
-public class RestaurantRepositoryImp implements RestaurantRepository {
+public class RestaurantRepositoryImpl implements RestaurantRepository {
 
     private final RestaurantJpaRepository restaurantJpaRepository;
     private final RestaurantDataAccessMapper restaurantDataAccessMapper;
 
-    public RestaurantRepositoryImp(RestaurantJpaRepository restaurantJpaRepository,
-                                   RestaurantDataAccessMapper restaurantDataAccessMapper) {
+    public RestaurantRepositoryImpl(RestaurantJpaRepository restaurantJpaRepository,
+                                    RestaurantDataAccessMapper restaurantDataAccessMapper) {
         this.restaurantJpaRepository = restaurantJpaRepository;
         this.restaurantDataAccessMapper = restaurantDataAccessMapper;
     }
-
 
     @Override
     public Optional<Restaurant> findRestaurantInformation(Restaurant restaurant) {
